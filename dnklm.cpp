7ec693ce2f24a474751f62ff076e7ee0036356f2
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-02-23 21:14:13 +0300

    Create dnklm.cpp

---
+++ dnklm.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+ #include <stdio.h> 
+
+  int x; 
+  char d;
+   int z; 
+   int sn; 
+   int s; 
+   char y; 
+   using namespace std; 
+   int main(){
+    //system ("color f0");
+     cout<<" DENKLEM UYGULAMASI"<<endl; 
+     while(true){
+     cout<<"bilinmeyen yerine x yaziniz"<<endl; 
+     cin>>y; 
+     cout<<" "; 
+     cin>>d;
+     cin>>z;
+     cout<<" = ";
+      cin>>s; 
+      int a; 
+      switch(y) {
+       case 'x':{ 
+       switch(d){ 
+       case '+': {
+        a= s - z ;
+         cout<<"x = "<<a<<endl; 
+         break;
+         } 
+         case '-': {
+          a= s + z ; 
+          cout<<"x = "<<a<<endl; 
+          break;
+          } 
+          case '*': {
+           a= s / z ; 
+           cout<<"x = "<<a<<endl; 
+           break;
+           } 
+           case '/': {
+            a= s * z ; 
+           
+
+            cout<<"x = "<<a<<endl;
+             break; 
+             } } } }}}
+
